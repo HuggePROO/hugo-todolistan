c869f5b93bb93b123d0b3514252daa5135865b1c
HuggePROO <hugo.larsson2@elev.ga.ntig.se>
2026-01-29 16:27:15 +0100

    Gjort lite saker

--- app.rb
+++ app.rb
@@ -1,22 +1,54 @@
-require 'debug'
-require "awesome_print"
-
 class App < Sinatra::Base
 
-    # Funktion för att prata med databasen
-    # Exempel på användning: db.execute('SELECT * FROM fruits')
-    def db
-      return @db if @db
+  
+  def db
+    return @db if @db
 
-      @db = SQLite3::Database.new(DB_PATH)
-      @db.results_as_hash = true
+    @db = SQLite3::Database.new("db/sqlite.db")
+    @db.results_as_hash = true
 
-      return @db
-    end
+    return @db
+  end
 
-    # Routen /
-    get '/' do
-        erb(:"index")
-    end
+  # Routen /
+  get '/' do
+    redirect(:"/movies")
+  end
+
+  get '/movies' do
+    @movies = db.execute('SELECT * FROM movies')
+    p @movies
+    erb(:"movies/index")
+  end
+  
+
+  get '/movies/new' do
+    erb(:"movies/new")
+  end
+
+  post '/movies' do
+    db.execute("INSERT INTO movies (name, poster, runtime, imdb) Values(?,?,?,?)", params.values)
+    redirect('/')
+  end
+
+  get '/movies/:id' do | id |
+    @movie = db.execute('SELECT * FROM movies WHERE id=?',id).first
+    erb(:"movies/show")
+  end
+
+  post '/movies/:id/delete' do | id |
+    db.execute("DELETE FROM movies WHERE id =?", id)
+    redirect("/")
+  end
+
+  get '/movies/:id/edit' do | id |
+    @movie = db.execute('SELECT * FROM movies WHERE id=?',id).first
+    erb(:"movies/edit")
+  end
+
+  post "/movies/:id/update" do | id |
+    db.execute("UPDATE movies SET name=?, poster=?, runtime=?, imdb=? WHERE id=?", params.values)
+    redirect("/")
+  end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -15,21 +15,30 @@ class Seeder
   end
 
   def self.drop_tables
-    db.execute('DROP TABLE IF EXISTS todos')
+    db.execute('DROP TABLE IF EXISTS movies')
   end
 
   def self.create_tables
-    db.execute('CREATE TABLE todos (
+    db.execute('CREATE TABLE movies (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
-                category_id INTEGER 
-                description TEXT)')
+                poster TEXT NOT NULL,
+                runtime TEXT NOT NULL,
+                imdb TEXT NOT NULL
+                )')
   end
 
   def self.populate_tables
-    db.execute('INSERT INTO todos (name, description) VALUES ("Köp mjölk", "3 lite mellanmjölk, eko")')
-    db.execute('INSERT INTO todos (name, description) VALUES ("Köp julgran", "En rödgran")')
-    db.execute('INSERT INTO todos (name, description) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Star Wars Revenge of the Sith", "https://storage.googleapis.com/pod_public/1300/266305.jpg", "2h 20m", "8,8")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("The Lord of the Rings: The Return of the King", "https://static.posters.cz/image/1300/133052.jpg", "3h 21m", "9,0")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Inception", "https://m.media-amazon.com/images/I/91b3Xtjt0IL._AC_UF1000,1000_QL80_.jpg", "2h 28m", "8,8")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("The Matrix", "https://m.media-amazon.com/images/I/51EG732BV3L._AC_.jpg", "2h 16m", "8,7")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Interstellar", "https://m.media-amazon.com/images/I/61ASebTsLpL._AC_UF894,1000_QL80_.jpg", "2h 49m", "8,6")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Avatar", "https://i.pinimg.com/736x/8b/2f/a6/8b2fa6fb94810cd0d335b479896f7fc8.jpg", "2h 42m", "7,9")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Guardians of the Galaxy", "https://m.media-amazon.com/images/I/81YZ8slCyuL.jpg", "2h 1m", "8,0")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Mad Max: Fury Road", "https://m.media-amazon.com/images/I/71rs1WWtoBL._AC_UF1000,1000_QL80_.jpg", "2h 0m", "8,1")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Dune: Part One", "https://i0.wp.com/schicksalgemeinschaft.wordpress.com/wp-content/uploads/2021/09/dune-part-one-poster.jpg?fit=810%2C1200&ssl=1", "2h 35m", "8,0")')
+    db.execute('INSERT INTO movies (name, poster, runtime, imdb) VALUES ("Pirates of the Caribbean: The Curse of the Black Pearl", "https://m.media-amazon.com/images/I/71zji3aER6L.jpg", "2h 23m", "8,1")')
   end
 
   private
